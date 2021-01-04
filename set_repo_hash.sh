220b00906ee4479eafb2561149079ed80e1dde4e
aimkiray <root@meowwoo.com>
2021-01-04 11:30:02 +0800

    fix: set env

--- set_repo_hash.sh
+++ set_repo_hash.sh
@@ -2,8 +2,8 @@
 name=$1
 if [ "$#" -gt 1 ]; then
     repo=$2
-    echo "::set-env name=${name}Hash::$(git ls-remote ${repo} HEAD | awk '{ print $1}' | cut -c -7)"
+    echo "::`echo -n name=${name}Hash::$(git ls-remote ${repo} HEAD | awk '{ print $1}' | cut -c -7)`::"
 else
     git log --invert-grep --author="action@github.com" -n 1
-    echo "::set-env name=${name}Hash::$(git log --invert-grep --author="action@github.com" -n 1 --pretty=format:%H | cut -c -7)"
+    echo "::`echo -n name=${name}Hash::$(git log --invert-grep --author="action@github.com" -n 1 --pretty=format:%H | cut -c -7)`::"
 fi
